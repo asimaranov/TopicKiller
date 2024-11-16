7b687ce3e6c1971531ab51c6ad27bd4f02251e52
asimaranov <a.simaranov@gmail.com>
2024-11-16 15:50:01 +0700

    Initial commit

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,47 @@
+import clientPromise from "@/app/lib/mongodb";
+
+export async function POST(request: Request) {
+  const data = await request.json();
+
+  if (!clientPromise) {
+    return Response.json({
+      status: "no-db",
+    });
+  }
+
+  const { segments, session_id } = data;
+
+  const client = await clientPromise;
+  const db = client.db(process.env.MONGODB_DB);
+
+  const sessionInfo = await db.collection("conversations").findOne({});
+
+  let conversations = sessionInfo ? [sessionInfo.conversation] : [];
+
+  for (const segment of segments) {
+    const text = segment.text;
+    const speaker = segment.speaker;
+    const speakerId = segment.speaker_id;
+    const isUser = segment.is_user;
+    const personId = segment.person_id;
+    const start = segment.start;
+    const end = segment.end;
+    conversations.push(text);
+  }
+
+  await db.collection("conversations").findOneAndUpdate(
+    { sessionId: session_id },
+    {
+      $set: {
+        conversation: conversations.join(" "),
+      },
+    },
+    {
+      upsert: true,
+    }
+  );
+
+  return Response.json({
+    status: "ok",
+  });
+}
